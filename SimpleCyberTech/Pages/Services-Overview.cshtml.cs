2a96160423e52ae63cd64706c65f3c8434ffde99
Michael Foss <michael.foss@simplecybertech.com>
2026-01-13 14:27:38 -0800

    Added Basic Pages
    Added the basic pages we will need to go live (templates/scaffolding).

---
+++ SimpleCyberTech/Pages/Services-Overview.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleCyberTech.Pages;
+
+public class ServicesOverviewModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ SimpleCyberTech/Pages/Services-Technology.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleCyberTech.Pages;
+
+public class ServicesTechnologyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
