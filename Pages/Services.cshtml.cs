4ba6ad386eac937622c4fc0be762de88f2f55964
Michael Foss <michael.foss@simplecybertech.com>
2026-01-29 13:15:47 -0800

    Update for Docker
    Updated the infrastructure to have the SLN in the same folder and added Docker support.

---
+++ Pages/Services.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace simplecybertech.Pages;
+
+public class ServicesModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
